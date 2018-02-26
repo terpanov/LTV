5303c584dfe93f57db7dab0a1b3b7802c94f5afe
denister <terpanov@gmail.com>
2018-02-26 01:05:30 -0800

    added Google start and end dates for entry date

--- appstore_calendar.py
+++ appstore_calendar.py
@@ -1,13 +1,15 @@
 """Apple and Google Payment Calendars
 Apple pulls calendar from Google Sheets
-Google is a pure function"""
+Google is a pure function
+Calendar outputs the next payment date from the app stores"""
 
 import pandas as pd
 from gspread_pandas import Spread
 import datetime
 import calendar
 
-# Apple Calendar
+
+# Get Apple start and end dates for entry date
 
 fiscal_calendar = Spread('LTV', 'fiscal_calendar')
 fiscal_calendar.open_sheet(0)
@@ -19,20 +21,22 @@ apple_fiscal['fiscal_months'] = pd.to_datetime(apple_fiscal['fiscal_month'])
 fiscal_months = apple_fiscal['fiscal_months']
 
 apple_fiscal['start_date'] = pd.to_datetime(apple_fiscal['start_date'])
-start_date = apple_fiscal['start_date']
+apple_start_date = apple_fiscal['start_date']
 
 apple_fiscal['end_date'] = pd.to_datetime(apple_fiscal['end_date'])
-end_date = apple_fiscal['end_date']
+apple_end_date = apple_fiscal['end_date']
 
 apple_fiscal['pay_date'] = pd.to_datetime(apple_fiscal['pay_date'])
-pay_date = apple_fiscal['pay_date']
+apple_pay_date = apple_fiscal['pay_date']
+
+# Apple Calendar
 
 def apple_paydate():
     date_entry = input('Enter Apple app store start date in YYYY-MM-DD format')
     year , month , day = map(int , date_entry.split ('-'))
     sample_date = datetime.date(year , month , day)
-    mask = (sample_date > start_date) & (sample_date <= end_date)
-    apple_pay = pd.to_datetime(pay_date.loc[mask].values[0], format='%Y-%m-%d', errors='coerce').date()
+    mask = (sample_date > apple_start_date) & (sample_date <= apple_end_date)
+    apple_pay = pd.to_datetime(apple_pay_date.loc[mask].values[0], format='%Y-%m-%d', errors='coerce').date()
     return apple_pay
 
 # Google Calendar
@@ -45,7 +49,19 @@ def google_paydate():
                                     calendar.monthrange(sample_date.year , sample_date.month)[ -1 ])
     return sample_date + datetime.timedelta(days=15)
 
-# Check
+# Get Google start and end dates for entry date
+
+date_entry = '2018-10-10'
+year , month , day = map (int , date_entry.split ('-'))
+sample_date = datetime.date (year , month , day)
+google_start_date = datetime.date(sample_date.year , sample_date.month , 1)
+_, num_days = calendar.monthrange(sample_date.year , sample_date.month)
+google_end_date = datetime.date(sample_date.year , sample_date.month , num_days)
+
+print(google_start_date)
+print(google_end_date)
+
+# Check if pay dates are workings
 
 print(apple_paydate())
 print(google_paydate())
