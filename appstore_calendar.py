9f976e387d19f00d34cd39cef9f1b2c7ba19a782
denister <terpanov@gmail.com>
2018-02-27 00:26:35 -0800

    added Google Calendar

--- appstore_calendar.py
+++ appstore_calendar.py
@@ -6,35 +6,49 @@ Calendar outputs the next payment date from the app stores"""
 import pandas as pd
 import datetime
 import calendar
+import numpy as np
 
-# Apple Calendar
+# Apple Pay Date
 
 def apple_paydate(date_entry):
     apple_fiscal = pd.read_csv ('/Users/plexchat/LTV_Geek/LTV_Files/apple_fiscal_calendar_2018_2021.csv' ,
                                 parse_dates=True , index_col=None)
-    apple_fiscal[ 'start_date' ] = pd.to_datetime (apple_fiscal[ 'start_date' ])
+    apple_fiscal[ 'start_date' ] = pd.to_datetime(apple_fiscal[ 'start_date' ])
     apple_start_date = apple_fiscal[ 'start_date' ]
-    apple_fiscal[ 'end_date' ] = pd.to_datetime (apple_fiscal[ 'end_date' ])
+    apple_fiscal[ 'end_date' ] = pd.to_datetime(apple_fiscal[ 'end_date' ])
     apple_end_date = apple_fiscal[ 'end_date' ]
-    apple_fiscal[ 'pay_date' ] = pd.to_datetime (apple_fiscal[ 'pay_date' ])
+    apple_fiscal[ 'pay_date' ] = pd.to_datetime(apple_fiscal[ 'pay_date' ])
     apple_pay_date = apple_fiscal[ 'pay_date' ]
     year , month , day = map(int , date_entry.split ('-'))
     sample_date = datetime.date(year , month , day)
     mask = (sample_date > apple_start_date) & (sample_date <= apple_end_date)
-    apple_pay = pd.to_datetime(apple_pay_date.loc[mask].values[0], format='%Y-%m-%d', errors='coerce').date()
-    return apple_pay, apple_start_date[0].date(), apple_end_date[0].date()
+    apple_pay = pd.to_datetime(apple_pay_date.loc[mask].values[0], format='%Y-%m-%d').date()
+    return apple_pay
 
-# Google Calendar
+# Google Pay Date
 
 def google_paydate(date_entry):
     year , month , day = map(int , date_entry.split('-'))
     sample_date = datetime.date(year , month , day)
-    sample_date = datetime.date(sample_date.year , sample_date.month ,
+    sample_date = datetime.date(sample_date.year, sample_date.month,
                                     calendar.monthrange(sample_date.year , sample_date.month)[ -1 ])
     google_start_date = datetime.date (sample_date.year , sample_date.month , 1)
     _ , num_days = calendar.monthrange (sample_date.year , sample_date.month)
     google_end_date = datetime.date (sample_date.year , sample_date.month , num_days)
-    return sample_date + datetime.timedelta(days=15), google_start_date, google_end_date
+    return sample_date + datetime.timedelta(days=1)
+
+# Google Pay Calendar
+
+def google_calendar(date_entry):
+    year , month , day = map(int , date_entry.split('-'))
+    sample_date = datetime.date(year , month , day)
+    start_datelist = pd.date_range(sample_date, periods=365).tolist()
+    start_google_table = pd.DataFrame(start_datelist)
+    start_google_table.index.name = 'days'
+    start_google_table.columns = ['start_date']
+    start_google_table.loc[ (start_google_table['start_date' ].dt.day == 15), 'pay_date'] = \
+        start_google_table[ 'start_date']
+    return start_google_table
 
 
 # Check if pay dates are workings
@@ -42,8 +56,9 @@ def google_paydate(date_entry):
 print(apple_paydate('2018-10-10'))
 print(google_paydate('2018-10-10'))
 
+# Check if calendar with pay dates are workings
 
-
+print(google_calendar('2018-10-10'))
 
 
 
